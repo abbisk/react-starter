a07a095ce34f174c30a4529e578fd7d31d7cf440
abbisk <abbiskpro@gmail.com>
2021-05-31 16:51:10 +0530

    comment component created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import './App.css';
 import faker from 'faker';
+import CommentDetail from './CommentDetail'
 
 function App() {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-      <div className="content">
-        <a href="/" className="author">
-        Abhishek
-      </a>
-      <div className="metadata">
-        <span className="date"> Today at 7:00PM</span>
-      </div>
-      <div classNmae="text">Nice blog post!</div>
-      </div>
-    </div>
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 }
