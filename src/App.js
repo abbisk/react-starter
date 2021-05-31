53fd3b9db8fe0ac8d774c4bd7a872ea6a184ca52
abbisk <abbiskpro@gmail.com>
2021-05-31 22:55:13 +0530

    content props added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import CommentDetail from './CommentDetail'
 function App() {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Abhishek" timeAgo="Today at 7:00PM"/>
-      <CommentDetail author="Anurag" timeAgo="Today at 6:00PM" />
-      <CommentDetail author="Ashvin" timeAgo="Today at 5:00PM" />
-      <CommentDetail author="Pankaj" timeAgo="Today at 4:00PM" />
-      <CommentDetail author="Akhil" timeAgo="Today at 3:00PM" />
+      <CommentDetail author="Abhishek" timeAgo="Today at 7:00PM" content="Nice blog post!" />
+      <CommentDetail author="Anurag" timeAgo="Today at 6:00PM" content="Nice blog" />
+      <CommentDetail author="Ashvin" timeAgo="Today at 5:00PM" content="Awesome blog post!" />
+      <CommentDetail author="Pankaj" timeAgo="Today at 4:00PM" content="Super post!" />
+      <CommentDetail author="Akhil" timeAgo="Today at 3:00PM" content="Simply awesome" />
     </div>
   );
 }
