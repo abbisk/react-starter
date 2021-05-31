7ab711f021324d461bbda1e8238c5ff885f6af15
abbisk <abbiskpro@gmail.com>
2021-05-31 22:49:52 +0530

    Passing multiple props

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import CommentDetail from './CommentDetail'
 function App() {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Abhishek" />
-      <CommentDetail author="Anurag" />
-      <CommentDetail author="Ashvin" />
-      <CommentDetail author="Pankaj" />
-      <CommentDetail author="Akhil" />
+      <CommentDetail author="Abhishek" timeAgo="Today at 7:00PM"/>
+      <CommentDetail author="Anurag" timeAgo="Today at 6:00PM" />
+      <CommentDetail author="Ashvin" timeAgo="Today at 5:00PM" />
+      <CommentDetail author="Pankaj" timeAgo="Today at 4:00PM" />
+      <CommentDetail author="Akhil" timeAgo="Today at 3:00PM" />
     </div>
   );
 }
