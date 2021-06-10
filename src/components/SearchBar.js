b27916ee20b177cd9c0539d782f2e6f932633575
abbisk <abbiskpro@gmail.com>
2021-06-10 18:40:02 +0530

    Solve Context issue

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,7 +7,7 @@ class SearchBar extends React.Component {
 
     state = { term: '' };
 
-    onFormSubmit(event){
+    onFormSubmit =event => {
         event.preventDefault();
         console.log(this.state.term);
     }
