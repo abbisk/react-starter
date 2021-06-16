13a5067e34da060096ed5ea999bf87eaf7ecba14
abbisk <abbiskpro@gmail.com>
2021-06-16 19:53:38 +0530

    Render list of images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import unsplash  from './api/unsplash';
 import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 
 
 class App extends React.Component {
@@ -20,8 +21,9 @@ class App extends React.Component {
     return(
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
+      
       
     );
   }
