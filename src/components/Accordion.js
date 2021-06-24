a6432ed64c740fdc16a0b877eb1199a9c7ad6e67
abbisk <abbiskpro@gmail.com>
2021-06-24 17:02:35 +0530

    widget added

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Accordion = ({ items }) => {
-    const renderItems = items.map(item => {
-        return <div key={item.title}>
-            <div className="title active">
+    const [activeIndex, setActiveIndex] = useState(null);
+
+
+    const onTitleClisk = (index) => {
+        setActiveIndex(index);
+    };
+
+    const renderItems = items.map((item, index) => {
+        const active = index === activeIndex ? 'active' : '';
+
+
+        return <React.Fragment key={item.title}>
+            <div 
+            
+            className={`title ${active}`}
+            onClick = {() => onTitleClisk(index)}
+            >
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
-            <div className="content active">
+            <div className={`content ${active}`}>
                 <p>{item.content}</p>
             </div>
-        </div>
+        </React.Fragment>
 
     })
-return  <div className="ui styled accordion">{renderItems}</div>
+return  <div className="ui styled accordion">{renderItems}
+
+</div>
 };
 
 export default Accordion;
